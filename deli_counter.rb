eaa0d787eec767d1184ba9e9dc76e423b5ea9a41
Dean L Betty <deanlloyd64@gmail.com>
2019-09-11 19:30:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ kat_deli = []
 
 def line(array)
   if array.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     # Show everyone's place in line
     queue = "The line is currently: "
