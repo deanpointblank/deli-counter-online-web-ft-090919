f80ba2f18ba695f3244682c1cbb81c26d88ad440
Dean L Betty <deanlloyd64@gmail.com>
2019-09-11 19:38:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
     # Show everyone's place in line
     queue = "The line is currently: "
     array.each do |person|
-      queue << "#{array(person) + 1} . #{person}"
+      queue << "#{array(person)} . #{person}"
     end
   puts queue
   end
