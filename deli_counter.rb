75e3ea9093215d3e57b2ff190c2313f073f12ef1
Dean L Betty <deanlloyd64@gmail.com>
2019-09-11 19:45:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
     i = 0
     queue = "The line is currently: "
     while i < array.length do
-      queue << " #{[i] + 1}. #{array[i]}"
+      queue << " #{i + 1}. #{array[i]}"
       i += 1
     end
   puts queue
