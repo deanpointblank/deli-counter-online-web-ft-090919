de017b6b44ce6104f9553f7e36f049738e68b6c0
Dean L Betty <deanlloyd64@gmail.com>
2019-09-11 19:16:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,11 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty"
   else
+    puts "The line is currently: "
     array.each do |person|
-      
+    puts 
 end
 
-def take_a_number(line, name)
+def take_a_number(array, name)
+  
 end
