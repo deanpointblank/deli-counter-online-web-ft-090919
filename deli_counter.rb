a1462efbc3b9b2a9abf671678c9ec91b3bc036ce
Dean L Betty <deanlloyd64@gmail.com>
2019-09-11 19:54:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,5 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array[name]} in line"
+  puts "Welcome, #{name}. You are number #{array} in line"
 end
